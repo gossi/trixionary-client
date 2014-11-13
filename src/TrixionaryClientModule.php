6f4c62eb5b4e1cc51beacf37d79c1b492764695b
gossi <github@gos.si>
2014-11-13 22:12:43 +0100

    init

---
+++ src/TrixionaryClientModule.php
@@ -0,0 +1,30 @@
+<?php
+namespace gossi\trixionary\client;
+
+use keeko\core\module\AbstractModule;
+
+/**
+ * Trixionary Client
+ * 
+ * @license MIT
+ * @author gossi
+ */
+class TrixionaryClientModule extends AbstractModule {
+
+	/**
+	 */
+	public function install() {
+	}
+
+	/**
+	 */
+	public function uninstall() {
+	}
+
+	/**
+	 * @param mixed $from
+	 * @param mixed $to
+	 */
+	public function update($from, $to) {
+	}
+}
